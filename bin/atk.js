aea4a00e4760a46f4f482dfa7ecfb9c534e28767
zrheaume <zachary.a.rheaume@gmail.com>
2019-02-14 19:22:28 -0500

    Improved CLI capability

--- bin/atk.js
+++ bin/atk.js
@@ -1,20 +1,9 @@
 #!/usr/bin/env node
 let toolkitLoaded = false
 try {
-   const atk = require("../lib/atk")
+   const atk = require("../lib/adomer-toolkit")
    const cmd = process.argv.slice(2)
-   console.log(cmd)
-   switch (process.argv[2]) {
-      case "init": case "-i":
-         atk.initializeCLIapp()
-         break;
-      case "help": case "-h":
-         console.log("open adomer-toolkit command list")
-         break;
-      default: 
-         console.log(`err: could not find arg. \ntry -help or -h to see a list of adomer commands`)
-         break;
-   }
+   atk.run(cmd)
 } catch (err) {
    console.log(err.stack)
 } finally {
